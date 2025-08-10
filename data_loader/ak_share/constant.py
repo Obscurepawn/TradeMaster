3f19aac2582b6131b4e2dd103a4377366909f008
obscurepawn <uestc.zyj@gmail.com>
2025-08-11 02:42:13 +0800

    support akshare dataloader

---
+++ data_loader/ak_share/constant.py
@@ -0,0 +1,80 @@
+CN_CODE = "股票代码"
+CN_NAME = "股票名称"
+CN_SHORT_CODE = "代码"
+CN_SHORT_NAME = "名称"
+CN_DATE = "日期"
+CN_OPEN = "开盘"
+CN_CLOSE = "收盘"
+CN_HIGH = "最高"
+CN_LOW = "最低"
+CN_VOLUME = "成交量"
+CN_AMOUNT = "成交额"
+CN_TURNOVER_RATE = "换手率"
+CN_AMPLITUDE = "振幅"
+CN_CHANGE_PCT = "涨跌幅"
+CN_CHANGE_AMT = "涨跌额"
+
+
+EN_CODE = "code"
+EN_NAME = "name"
+EN_DATE = "date"
+EN_OPEN = "open"
+EN_CLOSE = "close"
+EN_HIGH = "high"
+EN_LOW = "low"
+EN_VOLUME = "volume"
+EN_AMOUNT = "amount"
+EN_TURNOVER_RATE = "turnover_rate"
+EN_AMPLITUDE = "amplitude"
+EN_CHANGE_PCT = "change_pct"
+EN_CHANGE_AMT = "change_amt"
+
+MA5 = "ma5"
+MA10 = "ma10"
+MA20 = "ma20"
+MA60 = "ma60"
+
+DIF = "dif"
+DEA = "dea"
+MACD = "macd"
+
+RSI = "rsi"
+
+MIDDLE_BAND = "middle_band"
+UPPER_BAND = "upper_band"
+LOWER_BAND = "lower_band"
+
+
+VMA5 = "vma5"
+VMA10 = "vma10"
+
+K = "k"
+D = "d"
+J = "j"
+
+BIAS5 = "bias5"
+BIAS10 = "bias10"
+
+STOCK_HISTORY = "stock_history"
+CN_STOCK_ADJUST = "复权"
+EN_STOCK_ADJUST_QFQ = "qfq"
+EN_STOCK_ADJUST_HFQ = "hfq"
+PERIOD_DAILY = "daily"
+
+RENAME_DICT = {
+    CN_CODE: EN_CODE,
+    CN_NAME: EN_NAME,
+    CN_SHORT_NAME: EN_NAME,
+    CN_SHORT_CODE: EN_CODE,
+    CN_DATE: EN_DATE,
+    CN_OPEN: EN_OPEN,
+    CN_CLOSE: EN_CLOSE,
+    CN_HIGH: EN_HIGH,
+    CN_LOW: EN_LOW,
+    CN_VOLUME: EN_VOLUME,
+    CN_AMOUNT: EN_AMOUNT,
+    CN_TURNOVER_RATE: EN_TURNOVER_RATE,
+    CN_AMPLITUDE: EN_AMPLITUDE,
+    CN_CHANGE_PCT: EN_CHANGE_PCT,
+    CN_CHANGE_AMT: EN_CHANGE_AMT,
+}
