e7d79964f43f95bc99ccbb126725741a823295fe
obscurepawn <uestc.zyj@gmail.com>
2025-12-28 17:10:50 +0800

    reset this repo

--- config/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Configuration module for TradeMaster project.
-"""
--- data_loader/ak_share/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Akshare数据加载器模块
-"""
-
-from .akshare_data_loader import AkshareDataLoader
-
-__all__ = ['AkshareDataLoader']
